cd4451760d7e0d7c7f069cd916801ac4095f4ea5
ShubhamKashyap007 <151177857+ShubhamKashyap007@users.noreply.github.com>
2024-10-07 11:05:49 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,3 @@
+The manupulation code is to extract coordinates and making N*1 vector for training the model.
+The Training code does training for the dataset provided
+The part1results takes the trained model, and extracts and plots results.
